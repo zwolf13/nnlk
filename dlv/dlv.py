17b9eb6ecbf95315ad9615279bc6ac5638df0c0e
zwolf13 <zwolf13@gmail.com>
2022-10-19 19:27:37 -0400

    Updated

--- dlv/dlv.py
+++ dlv/dlv.py
@@ -3,18 +3,27 @@
 import youtube_dl
 
 ydl_opts = {
- 	'nocheckcertificate': True,
- 	'format': 'best[ext=mp4]/best',
- 	'nooverwrites': True,
- 	'noprogress': True,
- 	'restrictfilenames': True,
- 	'writeinfojson': True,
- 	'writethumbnail': True,
- 	'writesubtitles': True,
-#	'cookiefile': '',
-  'outtmpl': 'downloads/%(extractor)s/%(title)s - %(id)s.%(ext)s',
-	 'download_archive': 'dev-archive.txt'
+    'nocheckcertificate': True,
+    'format': 'best[ext=mp4]/best',
+    'nooverwrites': True,
+    'noprogress': True,
+    'restrictfilenames': True,
+    'writeinfojson': True,
+    'writethumbnail': True,
+    'writesubtitles': True,
+    'cookiefile': 'cookies/youtube.txt',
+    'outtmpl': 'downloads/%(extractor)s/%(title)s - %(id)s.%(ext)s',
+    'download_archive': 'dev-archive.txt'
 }
 
+urls = []
+with open('urls.txt') as f:
+    urls = f.read().splitlines()
+
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-  ydl.download(['https://www.tiktok.com/t/ZTRUruv4j/?k=1'])
+    for url in urls:
+        print(f'Working on: {url}')
+        try:
+            ydl.download([url])
+        except:
+            print(f"An exception occurred with url '{url}'") 
