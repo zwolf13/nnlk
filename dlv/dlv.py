6a7c9b0944b28297e60e92ed1d07848991863035
zwolf13 <zwolf13@gmail.com>
2022-10-29 17:18:49 -0400

    Refactored
     - Added main()
     - Updated print_usage()
     - Added print_version()
     - Added _handle_argv()
     - Added _append_url()

--- dlv/dlv.py
+++ dlv/dlv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # coding: utf-8
 
 import sys
@@ -13,7 +14,6 @@ from youtube_dl import YoutubeDL
 # TODO
 #  - Add a way to get the status: (different script?)
 #      - Is running, cat log, entries in input urls.txt, number of failures, disk check (initial, current and final)
-#  - Add disk free space check
 #  - Move write_file and loadUrls methods to a utility
 #  - Add print usage
 
@@ -35,13 +35,52 @@ BACKUP_FOLDER = None
 OUTPUT_FOLDER = None
 
 
+def main(argv: list[str]) -> None:
+    _init()
+    urls = _handle_argv(argv)
+    if not urls:
+        urls = load_urls()
+
+    total_urls = len(urls)
+    if total_urls < 1:
+        log.error('No URLs found :(')
+        sys.exit(1)
+
+    write_file(urls, BACKUP_FOLDER, f'{EXEC_TIME}-input.txt')
+
+    global COUNTER
+    with YoutubeDL(_get_ytdl_opts()) as ydl:
+        for url in urls:
+            COUNTER += 1
+            log.info(f"Working on {COUNTER} of {total_urls}: '{url}'")
+            try:
+                # TODO Refactor this to:
+                #  1. Download metadata
+                #  2. Determine extractor-specific parameters
+                #  3. Check estimated video size vs disk space (add disk space check)
+                #  4. Download video
+                ydl.download([url])
+                SUCCESS.append(url)
+            except:
+                log.error(f"An exception occurred with url '{url}'")
+                FAILURES.append(url)
+
+    write_file(SUCCESS, BACKUP_FOLDER, f'{EXEC_TIME}-success.txt')
+    write_file(FAILURES, BACKUP_FOLDER, f'{EXEC_TIME}-failures.txt')
+    print_summary()
+
+
 def print_usage() -> None:
-    print('dlv.py [-i <input_file>] [-o <output_file>]')
+    print('dlv.py [-i <INPUT_FILE>] [-o <OUTPUT_FILE>] [<URLs>]')
 
 
-def init() -> None:
-    """Initializes script parameters"""
-    log.info('Loading DLV config')
+def print_version() -> None:
+    print(f'DLV v{VERSION}')
+
+
+def _init() -> None:
+    """Initializes default config"""
+    log.debug('Loading DLV config')
     global HOST
     global BACKUP_FOLDER
     global OUTPUT_FOLDER
@@ -61,29 +100,67 @@ def init() -> None:
     OUTPUT_FOLDER = dlv_params.get('output_folder')
 
 
-def load_urls() -> list:
+def _handle_argv(argv: list[str]) -> list[str]:
+    """Handles input parameters to override default config.
+    If URLs where typed, they will be validated and a list of unique entries will be returned."""
+    log.debug('Handling input args')
+
+    try:
+        opts, args = getopt.getopt(
+            argv, 'hi:o:', ['help', 'version', 'input-file=', 'output-file='])
+    except getopt.GetoptError:
+        log.error('Invalid parameters! :(')
+        print_usage()
+        sys.exit(1)
+
+    # Overloads default config
+    global INPUT_URLS_FILE
+    global OUTPUT_FOLDER
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print_usage()
+            sys.exit(0)
+        elif opt in ('--version'):
+            print_version()
+            sys.exit(0)
+        elif opt in ('-i', '--input-file'):
+            INPUT_URLS_FILE = arg
+        elif opt in ('-o', '--output-file'):
+            OUTPUT_FOLDER = arg
+
+    urls = []
+    if args and len(args) > 0:
+        for url in args:
+            _append_url(urls, url)
+
+    return urls
+
+
+def _append_url(urls: list, url: str) -> None:
+    """Adds the new URL to given list if it is valid and it wasn't previously added to the list"""
+    if not validators.url(url):
+        log.warning(f'Skipping invalid URL: "{url}"')
+    elif url in urls:
+        log.warning(f'Skipping duplicate URL: "{url}"')
+    else:
+        log.debug(f'Appending "{url}"')
+        urls.append(url)
+
+
+def load_urls() -> list[str]:
     """Loads a list of URLs contained in urls.txt file."""
     log.info('Loading URLs')
     urls = []
 
     with open(INPUT_URLS_FILE) as urls_file:
         for line in urls_file:
-            url = line.strip()
-            if not validators.url(url):
-                log.warning(f'Skipping invalid URL: "{url}"')
-            elif url in urls:
-                log.warning(f'Skipping duplicate URL: "{url}"')
-            else:
-                urls.append(url)
+            _append_url(urls, line.strip())
 
     return urls
 
 
-def get_ytdl_opts(extractor=None) -> dict:
-    log.info('Getting YouTubeDL options')
-
-    # TODO - Config number of retries
-    log.debug('Loading default opts')
+def _get_ytdl_opts(extractor=None) -> dict:
+    log.debug('Getting YouTubeDL options')
     opts = None
     with open('default-opts.json') as file:
         opts = json.load(file)
@@ -126,79 +203,14 @@ def write_file(content, folder: str, filename: str, is_json=False) -> None:
 
 
 def print_summary() -> None:
-    log.info(f'------------------------------')
+    log.info(f'--------------------------------------------------')
     log.info(f'DLV VERSION    {VERSION}')
     log.info(f'OUTPUT_FOLDER  {OUTPUT_FOLDER}')
     log.info(f'SUCCESS        {len(SUCCESS)}')
     log.info(f'FAILURES       {len(FAILURES)}')
     log.info(f'TOTAL          {COUNTER}')
-    log.info(f'------------------------------')
+    log.info(f'--------------------------------------------------')
 
 
-# TODO
-# def main():
-#     print('all starts here')
-#
-# if __name__ == "__main__":
-#    main(sys.argv[1:])
-
-
-# TODO Check for an input list before loading URLs from file
-# TODO Fix long-name opts
-# https://www.tutorialspoint.com/python/python_command_line_arguments.htm
-urls = []
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'hi:o:u:', [
-                               'input-file=', 'output-file=', 'url='])
-except getopt.GetoptError:
-    log.error('Invalid parameters! :(')
-    print_usage()
-    sys.exit(1)
-
-for opt, arg in opts:
-    if opt in ('-h', '--help'):
-        print_usage()
-        sys.exit(0)
-    elif opt in ('-i', '--input-file'):
-        inputfile = arg
-    elif opt in ('-o', '--output-file'):
-        outputfile = arg
-    elif opt in ('-u', '--url'):
-        if not validators.url(arg):
-            log.warning(f'Skipping invalid URL: "{arg}"')
-        else:
-            urls.append(arg)
-
-
-init()
-if len(urls) < 1:
-    urls = load_urls()
-
-total_urls = len(urls)
-if total_urls < 1:
-    log.error('No URLs found :(')
-    sys.exit(1)
-else:
-    log.info(f'{total_urls} unique URLs found')
-
-write_file(urls, BACKUP_FOLDER, f'{EXEC_TIME}-input.txt')
-
-with YoutubeDL(get_ytdl_opts()) as ydl:
-    for url in urls:
-        COUNTER += 1
-        log.info(f"Working on {COUNTER} of {total_urls}: '{url}'")
-        try:
-            # TODO Refactor this to:
-            #  1. Download metadata
-            #  2. Determine extractor-specific parameters
-            #  3. Check estimated video size vs disk space
-            #  4. Download video
-            ydl.download([url])
-            SUCCESS.append(url)
-        except:
-            log.error(f"An exception occurred with url '{url}'")
-            FAILURES.append(url)
-
-write_file(SUCCESS, BACKUP_FOLDER, f'{EXEC_TIME}-success.txt')
-write_file(FAILURES, BACKUP_FOLDER, f'{EXEC_TIME}-failures.txt')
-print_summary()
+if __name__ == "__main__":
+    main(sys.argv[1:])
