b3fcd8bd1151d7a9b0fc41a499ccb42595c3bd79
zwolf13 <zwolf13@gmail.com>
2022-11-11 21:37:18 -0500

    Update utils.py

--- nnlk/commons/utils.py
+++ nnlk/commons/utils.py
@@ -24,13 +24,12 @@ def get_logger(name) -> logging.Logger:
 
 
 def load_config(file=NNLK_CONFIG, section='default'):
-    section_config = None
+    section_config = {}
     config = configparser.ConfigParser()
     config.read(file)
 
     if section not in config.sections():
         LOG.error(f'No section "{section}" found in "{file}" :(')
-        # TODO - Raise error
     else:
         section_config = config[section]
 
