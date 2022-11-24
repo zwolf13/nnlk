38251d10c6c5c35d8356e4562ba29abb3c92f691
zwolf13 <zwolf13@gmail.com>
2022-11-24 16:21:55 -0500

    Update dlv-ws.py

--- nnlk/dlv/dlv-ws.py
+++ nnlk/dlv/dlv-ws.py
@@ -2,8 +2,11 @@
 # coding: utf-8
 
 from flask import Flask, request
-# https://docs.python.org/3/reference/import.html?highlight=relative%20imports#package-relative-imports
-from nnlk.dlv.dlv import main
+from datetime import datetime
+import threading
+from nnlk.commons.constants import UNDERSCORE_DATE
+import nnlk.commons.utils as utils
+import nnlk.dlv.dlv as dlv
 import nnlk.dlv.finder as finder
 
 # https://flask.palletsprojects.com/
@@ -12,24 +15,65 @@ app = Flask(__name__)
 # USAGE: python -m flask --app dlv-ws [--debug] run
 
 # TODO
-#  - Connect to dlv.py
+#  - Add swagger
 #  - Start/Stop downloader, queue empty => wait for elements
 #  - Maintain a queue of URLs
 #     - Add URL to queue
 #  - Retry last failures
+#  - Get logs
+
+LOG = utils.get_logger('DLV-WS')
+
+# Script variables
+EXEC_TIME = datetime.now().strftime(UNDERSCORE_DATE)
+
+# Config variables
+VERSION = None
+HOST = None
+BACKUP_FOLDER = None
+OUTPUT_FOLDER = None
+COOKIE = None
+
+
+@app.before_first_request
+def _init() -> None:
+    """Initializes default config"""
+    global VERSION
+    global HOST
+    global BACKUP_FOLDER
+    global OUTPUT_FOLDER
+    config = utils.load_config()
+    VERSION = config.get('version')
+    HOST = config.get('host')
+    BACKUP_FOLDER = config.get('backup_folder')
+    OUTPUT_FOLDER = config.get('output_folder')
+    dlv._init()
 
 
 @app.route('/dlv/version', methods=['GET'])
-def status() -> dict:
-    return {'version': 'TODO - Return version from .ini file'}
+def get_version() -> dict[str, str]:
+    return {'version': VERSION}
 
 
 @app.route('/dlv/download', methods=['GET'])
-def test() -> dict[str, str]:
-    url = request.args.get('url')
-    # TODO Run main asynchonously
-    main([url])
-    return {'status': 'OK', 'url': f'{url}'}
+def download() -> dict[str, str]:
+    status = None
+    message = None
+    url = None
+
+    try:
+        url = request.args.get('url')
+        my_thread = threading.Thread(target=dlv.download_files, args=([url],))
+        my_thread.start()
+    except Exception as e:
+        LOG.error(f'An exception occurred with url "{url}": "{e}"')
+        status = 'ERROR'
+        message = str(e)
+    else:
+        status = 'OK'
+        message = f'Started download: {url}'
+    
+    return {'status': status, 'message': message}
 
 
 @app.route('/dlv/search', methods=['GET'])
